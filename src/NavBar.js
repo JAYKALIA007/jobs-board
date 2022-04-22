e056a04a617a9b3a6c77ed6d068a1be132197f02
Jay Kalia <1895567@tcs.com>
2022-04-22 23:05:30 +0530

    Revert "added auth0"
    This reverts commit e7dfb333badb882a1bca8137a4f8260f8ffae20c.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,17 @@
 import { Link } from 'react-router-dom'
-import LoginButton from './Auth/LoginButton'
-import LogoutButton from './Auth/LogoutButton'
-import { useAuth0 } from '@auth0/auth0-react'
 export default function NavBar(){
-    const { isLoading } = useAuth0();
     return(
         <div className="max-w-full text-gray-200 bg-gradient-to-r from-blue-700 to-blue-300  p-5 flex " >
             <div className=" w-1/2 m-2 " >
                 <Link to="/" className="text-2xl " >Jobs Board</Link>
             </div>
-            <div className=" w-1/2 m-2 text-right" >
+            <div className=" w-1/2 m-2 " >
                 {/* style the navbar with grid cols so that it's responsive */}
-                {!isLoading && (
-                    <div>
-                        <LoginButton />
-                        <LogoutButton />
-                    </div>
-                )}
-                {isLoading && (<p className="text-underline px-10 hover:underline underline-offset-2">Loading...</p>)}
+                <a href="http://localhost:3000/#" className="text-underline px-10 hover:underline underline-offset-2"> Link 1 </a>
+                <a href="http://localhost:3000/#" className="text-underline px-10 hover:underline underline-offset-2"> Link 2 </a>
+                <a href="http://localhost:3000/#" className="text-underline px-10 hover:underline underline-offset-2"> Link 3 </a>
+                <a href="http://localhost:3000/#" className="text-underline px-10 hover:underline underline-offset-2"> Link 4 </a>
+
             </div>
         </div>
     )
