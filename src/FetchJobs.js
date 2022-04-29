76ace017e371f14bd3e186b8485f59ed71c3ffbc
Jay Kalia <1895567@tcs.com>
2022-04-29 21:00:32 +0530

    added next/prev to home page,next to categories&searchterm

--- src/FetchJobs.js
+++ src/FetchJobs.js
@@ -7,6 +7,7 @@ export default function FetchJobs(){
     const [jobs, setJobs] = useState([]);
     const [filterIsActive, setFilterIsActive] = useState(false);
     const[ jobsFound , setJobsFound] = useState(true)
+    const [offsetValue, setOffsetValue] = useState(0)
     function callback(response){
         if(response === 'No category selected')
         {
@@ -15,7 +16,6 @@ export default function FetchJobs(){
         else{
             setFilterIsActive(true)
             if(response === 'No jobs found. Please change search term.'){
-                // setJobs([])
                 setJobsFound(false)
             }
             else{
@@ -26,20 +26,46 @@ export default function FetchJobs(){
     }
     useEffect(()=>{
         if(!filterIsActive){
-            axios.get(`https://remotive.io/api/remote-jobs`)
+            axios.get(`https://remotive.io/api/remote-jobs?limit=10&offset=${offsetValue}`)
             .then(res=>{
-                setJobs(res.data.jobs);
+                setJobs(res.data.jobs)
             })
             .catch(err=>{
                 console.log(err)
             })
         }
-    },[filterIsActive])
+    },[filterIsActive , offsetValue])
 
+    function handleNextPage(){
+        setOffsetValue(offsetValue+10)
+        window.scrollTo(0, 0);
+    }
+    function handlePrevPage(){
+        setOffsetValue(offsetValue-10)
+        window.scrollTo(0, 0);
+    }
     return(
         <div className="flex" >
             <div className="w-3/4 ">
-                {jobsFound && <Jobs jobs={jobs}/>}
+                {jobsFound && 
+                <> 
+                    <Jobs jobs={jobs}/>
+                    <div className="text-center" >
+                        <button className="bg-gradient-to-r from-blue-600 to-blue-400 
+                                         hover:from-blue-900 hover:to-blue-600 ...  px-3 py-1  
+                                           rounded-md  hover:scale-105 duration-100 text-gray-100 my-5 mx-2 " 
+                                onClick={handleNextPage}
+                        >
+                            Next
+                        </button>
+                        <button className={`text-gray-100 px-3 py-1 rounded-md  m-1 ${ offsetValue>0 ? "bg-gradient-to-r from-blue-600 to-blue-400 hover:from-blue-900 hover:to-blue-600    hover:scale-105 duration-100" : "bg-blue-300"}`}
+                                disabled={offsetValue <= 0} 
+                                onClick={handlePrevPage}
+                        >
+                            Back
+                        </button>
+                    </div>
+                </> }
                 {!jobsFound && (
                     <div className="px-12 py-32 mx-20  my-12 font-bold italic " >
                         <h4 className="text-6xl text-slate-700" >No jobs found.</h4>
